811bc5594e61962ac9849425e0d204b7cab99d96
Pranjali Rathi <pranjalirathi05@gmail.com>
2023-11-01 13:17:53 +0530

    setup

---
+++ codit/src/components/Landing.jsx
@@ -0,0 +1,15 @@
+import Navbar from './Navbar'
+
+
+const Landing = () => {
+    return (
+        <div>
+            <Navbar/>
+            hello from landing page
+        
+        </div>
+        
+    )
+}
+
+export default Landing;
---
+++ codit/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+//Code for the navigation bar
+
+const Navbar = () => {
+
+    return (
+        <div>
+            this is the code for navigation bar
+        </div>
+    )
+}
+
+export default Navbar;
